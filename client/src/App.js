ea88c46e53223b76375c2beaa042b596ca36f331
Fadin Artem <a.fadin-forwork@mail.ru>
2024-08-02 00:25:12 +0500

    init all

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import LongPooling from './longpooling';
+import EventSourcing from './eventsourcing';
+import WebSock from './websocket';
+
+function App() {
+  return (
+    <div>
+      {/* <LongPooling/> */}
+      {/* <EventSourcing/> */}
+      <WebSock/>
+    </div>
+  );
+}
+
+export default App;
